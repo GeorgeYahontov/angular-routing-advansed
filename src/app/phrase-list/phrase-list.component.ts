e10905567ebe90f9b9a4731ed8f153e8629605b2
George <yaxontov1995@gmail.com>
2021-08-26 20:40:00 +0300

    add modules, servises and components

---
+++ src/app/phrase-list/phrase-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-phrase-list',
+  templateUrl: './phrase-list.component.html',
+  styleUrls: ['./phrase-list.component.scss']
+})
+export class PhraseListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
